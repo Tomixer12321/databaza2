f8695c320509a27c7cc9fa8180709b727e760175
Tomixer <tomix.klokar@gmail.com>
2023-11-24 18:56:35 +0100

    vytvorenie SharedLayout.js a pridanie do neho Footer a Navbar

---
+++ src/pages/SharedLayout.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Navbar"
+
+const SharedLayout = () => {
+  return <>
+    <Navbar />
+    <Footer />
+  </>
+}
+
+export default SharedLayout
