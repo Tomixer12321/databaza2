47146ad7cf6e5f90df37dc33d54dd97bccbfd3ab
Tomixer <tomix.klokar@gmail.com>
2023-11-28 18:52:51 +0100

    kazdy film ma svoju stranku

--- src/pages/OneMovie.js
+++ src/pages/OneMovie.js
@@ -1,9 +1,32 @@
 import { useParams } from "react-router-dom"
+import { useState,useEffect } from "react"
+import projectFirestore from "../firebase/Config"
 
 const OneMovie = () => {
+  const [data,setData]=useState({})
+  const [error,setError]=useState(false)
+
   const {movieId}=useParams()
+
+  useEffect(()=>{
+    projectFirestore.collection("movies").doc(movieId).get().then((document)=>{
+      console.log(document)
+      if(document.exists){
+        setData(document.data())
+      }else{
+        setError("nenasiel sa film")
+      }
+    }).catch((err)=>{
+      setError(err.message)
+    })
+  },[])
+
   return (
-    <div>{movieId}</div>
+    <section>
+      <h1>{data.title}</h1>
+      <p>{data.minage}+</p>
+      <p>{data.time}min</p>
+    </section>
   )
 }
 
